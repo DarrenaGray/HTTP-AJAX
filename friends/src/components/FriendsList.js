64eacf128f0d303e844c88f0a74bf012b142703d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-03 17:39:43 -0400

    Import and route for friend component. Update friend and delete friend methods created

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
-const FriendsList = props => {
-    const { name, age, email } = props.friends;
-    return (
-        <div className="friendCardInfo">
-            <h1>{name}</h1>
-            <p><span>age:</span> {age}</p>
-            <p><span>email:</span> {email}</p>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-        
-    )
+class FriendsList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friend: {
+                name: '',
+                age: '',
+                email: ''
+            }
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            friend: {
+                ...this.state.friend,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    render() {
+        const { name, age, email, id } = this.props.friends;
+        return (
+            <div className="friendCardInfo">
+                <Link to={`/friends/${id}`}>
+                    <h1>{name}</h1>
+                    <p><span>age:</span> {age}</p>
+                    <p><span>email:</span> {email}</p>
+                </Link>
+            </div>
+            
+        )
+    }
 }
 
 
