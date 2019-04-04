5f9e4e97bbe70acdfc3d1176ca2b5b0234d34b77
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-04 16:51:52 -0400

    Added update and delete friend fns. onClick for update friend successfully redirects to update friend form with current friend info, onClick for delete friend successfully deletes friend and redirects back to friends list after delete

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,19 +3,32 @@ import React from 'react'
 //issues with array not repopulating on page refresh. returns an undefined error - TypeError: Cannot read property 'name' of undefined
 
 const Friend = props => {
+  console.log(props.friends)
   const id = props.match.params.id
   const friend = props.friends.find(friend => {
-    console.log(friend, id);
     return `${friend.id}` === id;
-  })
+  });
+
+  const updateFriend = e => {
+    e.preventDefault();
+    props.setActiveFriend(friend);
+    props.history.push('/update-friend');
+  }
+
+  const deleteFriend = e => {
+    e.preventDefault();
+    props.deleteFriend(friend);
+    props.history.push('/friends')
+  }
+
   return (
     <div>
         <h3>{friend.name}</h3>
         <p>{friend.age}</p>
         <p>{friend.email}</p>
-        <button onClick={e => props.setUpdateForm(e, friend)}>Update</button>
+        <button onClick={updateFriend}>Update</button>
         {/* delete button works but error returns due to issues with array not populating on page refresh */}
-        <button onClick={e => props.deleteFriend(e, friend.id)}>Delete</button>
+        <button onClick={deleteFriend}>Delete</button>
     </div>
   )
 }
