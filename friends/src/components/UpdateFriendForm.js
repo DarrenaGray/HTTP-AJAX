e2ef7216ed603fd2f4df78f7c2bdcd45ceccaf8d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-04 16:49:00 -0400

    Added an update friend form component for updating friend info. Friend info successfully updates on button click/submit

---
+++ friends/src/components/UpdateFriendForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+ class UpdateFriendForm extends React.Component {
+     state = {
+       friend: this.props.activeFriend
+       };
+    
+
+
+     changeHandler = e => {
+       // e.persist() allows the input value to change. Using e.preventDefault() or using no e.persist() returns a TypeError: Cannot read property 'name' of null
+       e.persist();
+       this.setState(prevState => ({
+          friend: {
+            ...prevState.friend,
+            [e.target.name]: e.target.value
+          }
+       }));
+     }
+
+
+     handleSubmit = e => {
+       e.preventDefault();
+       this.props.updateFriend(this.state.friend);
+     }
+     
+  render() {
+    return (
+      <div>
+        <h1>Update Friend</h1>
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" name="name" onChange={this.changeHandler} value={this.state.friend.name} placeholder="Name" required/>
+            <input type="number" name="age" onChange={this.changeHandler} value={this.state.friend.age} placeholder="Age"required/>
+            <input type="email" name="email" onChange={this.changeHandler} value={this.state.friend.email} placeholder="Email" required/>
+            <button type="submit">Update Friend</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default UpdateFriendForm;
